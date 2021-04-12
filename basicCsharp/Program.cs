c805140dbc4d3f3c79721cbda1287c33dcf13a81
ibrahim <mohdi9003@gmail.com>
2021-04-12 16:47:23 +0530

    add new

--- basicCsharp/Program.cs
+++ basicCsharp/Program.cs
@@ -7,6 +7,7 @@ namespace basicCsharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("git");
             Console.WriteLine("       /|");
             Console.WriteLine("      / |");
             Console.WriteLine("     /  |");
