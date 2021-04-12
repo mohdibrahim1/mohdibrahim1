aedc201dd27f9457d9af398e35ca304533272e1a
ibrahim <mohdi9003@gmail.com>
2021-04-12 16:26:03 +0530

    first commit

---
+++ basicCsharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace basicCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("       /|");
+            Console.WriteLine("      / |");
+            Console.WriteLine("     /  |");
+            Console.WriteLine("    /   |");
+            Console.WriteLine("    ----");
+            Console.WriteLine("Hello World!");
+            string charname = "don chandu";
+            Console.WriteLine("once was a man his name is " + charname);
+            Console.WriteLine("he is 70 year old");
+            Console.WriteLine("he was like his name gulfam");
+            Console.WriteLine("he was didn't like his age 70 years old");
+        }
+    }
+}
